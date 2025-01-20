31f604ac1705b09ea76537dfefd7254f88a4fb2a
ianroddrs <ianroddrs@gmail.com>
2025-01-20 14:11:06 -0300

    refatora geração de QR Code para pagamentos PIX e implementa nova classe Pix

---
+++ apps/main/pix.py
@@ -0,0 +1,73 @@
+import crcmod
+import qrcode
+import io
+import base64
+
+class Pix():
+    def __init__(self, valor):
+        
+        self.nome = 'Ian Mateus Alves Rodirgues'
+        self.chavepix = '+5591989055041'
+        self.valor = str(valor).replace(',', '.')
+        self.cidade = 'Ananindeua'
+        self.txtId = 'FinanControl'
+
+        self.nome_tam = len(self.nome)
+        self.chavepix_tam = len(self.chavepix)
+        self.valor_tam = len(self.valor)
+        self.cidade_tam = len(self.cidade)
+        self.txtId_tam = len(self.txtId)
+
+        self.merchantAccount_tam = f'0014BR.GOV.BCB.PIX01{self.chavepix_tam:02}{self.chavepix}'
+        self.transactionAmount_tam = f'{self.valor_tam:02}{float(self.valor):.2f}'
+
+        self.addDataField_tam = f'05{self.txtId_tam:02}{self.txtId}'
+
+        self.nome_tam = f'{self.nome_tam:02}'
+
+        self.cidade_tam = f'{self.cidade_tam:02}'
+
+        self.payloadFormat = '000201'
+        self.merchantAccount = f'26{len(self.merchantAccount_tam):02}{self.merchantAccount_tam}'
+        self.merchantCategCode = '52040000'
+        self.transactionCurrency = '5303986'
+        self.transactionAmount = f'54{self.transactionAmount_tam}'
+        self.countryCode = '5802BR'
+        self.merchantName = f'59{self.nome_tam:02}{self.nome}'
+        self.merchantCity = f'60{self.cidade_tam:02}{self.cidade}'
+        self.addDataField = f'62{len(self.addDataField_tam):02}{self.addDataField_tam}'
+        self.crc16 = '6304'
+
+  
+    def gerarPayload(self):
+        self.payload = f'{self.payloadFormat}{self.merchantAccount}{self.merchantCategCode}{self.transactionCurrency}{self.transactionAmount}{self.countryCode}{self.merchantName}{self.merchantCity}{self.addDataField}{self.crc16}'
+
+        self.gerarCrc16(self.payload)
+
+    
+    def gerarCrc16(self, payload):
+        crc16 = crcmod.mkCrcFun(poly=0x11021, initCrc=0xFFFF, rev=False, xorOut=0x0000)
+
+        self.crc16Code = hex(crc16(str(payload).encode('utf-8')))
+
+        self.crc16Code_formatado = str(self.crc16Code).replace('0x', '').upper().zfill(4)
+
+        self.payload_completa = f'{payload}{self.crc16Code_formatado}'
+
+        self.gerarQrCode(self.payload_completa)
+
+    
+    def gerarQrCode(self, payload):
+        self.qrcode = qrcode.make(payload)
+
+        img_buffer = io.BytesIO()
+
+        self.qrcode.save(img_buffer, format="PNG")
+
+        img_buffer.seek(0)
+        
+        self.qrcode = base64.b64encode(img_buffer.read()).decode('utf-8')
+
+    def getPix(self):
+        self.gerarPayload()
+        return {'qrcode': self.qrcode, 'payload': self.payload_completa}
